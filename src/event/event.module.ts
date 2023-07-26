6b8f90ec427afa2496c051dc9c2eeae809013ddb
Mehdi Gorbanzadeh <M.Gorbanzadeh@parlar.com>
2023-07-26 14:52:56 +0330

    add bugsnag and change listeners on function

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,13 +1,12 @@
 import { Module } from "@nestjs/common";
 
-import { Web3Module } from "src/web3/web3.module";
-import { LastState, LastStateSchema } from "./schemas";
-import { LastStateRepository } from "./lastState.repository";
 import { MongooseModule } from "@nestjs/mongoose";
-import { Listener } from "./listener/listener.event";
+import { BugsnagModule } from "src/bugsnag/bugsnag.module";
+import { Web3Module } from "src/web3/web3.module";
 import { EventService } from "./event.service";
-
-
+import { LastStateRepository } from "./lastState.repository";
+import { Listener } from "./listener/listener.event";
+import { LastState, LastStateSchema } from "./schemas";
 
 @Module({
   imports: [
@@ -15,13 +14,10 @@ import { EventService } from "./event.service";
     MongooseModule.forFeature([
       { name: LastState.name, schema: LastStateSchema },
     ]),
+    BugsnagModule,
   ],
   controllers: [],
-  providers: [
-    Listener,
-    EventService,
-    LastStateRepository,
-  ],
+  providers: [Listener, EventService, LastStateRepository],
   exports: [],
 })
 export class EventModule {}
--- src/event/listener/listener.event.ts
+++ src/event/listener/listener.event.ts
@@ -1,39 +1,41 @@
 import { Injectable } from "@nestjs/common";
 
-import * as LensEvents from "./contracts/LensEvents.json";
 import * as ForestFeedEvents from "./contracts/ForestFeedEvents.json";
+import * as LensEvents from "./contracts/LensEvents.json";
 
 import { ConfigService } from "@nestjs/config";
-import { Web3Service } from "src/web3/web3.service";
-import { ForestFeedEventName, LastStateIds, LensEventName } from "src/common/constants";
 import { Command, Option } from "nestjs-command";
 import { BugsnagService } from "src/bugsnag/bugsnag.service";
+import {
+  ForestFeedEventName,
+  LastStateIds,
+  LensEventName,
+} from "src/common/constants";
+import { Web3Service } from "src/web3/web3.service";
 import { EventService } from "../event.service";
 
 const EthereumEvents = require("ethereum-events");
 
-
 export const ListenerConfig = {
-  POLL_INTERVAL:13000,
-  CONFIRMATIONS:12,
-  CHUNK_SIZE:1,
-  CONCURRENCY:10,
-  BACK_OFF:1000,
-}
+  POLL_INTERVAL: 13000,
+  CONFIRMATIONS: 12,
+  CHUNK_SIZE: 1,
+  CONCURRENCY: 10,
+  BACK_OFF: 1000,
+};
 
-export const ListenerType =  {
-  balance:"runForestFeedEventListener",
-  mirror:"runLensEventListener",
-}
+export const ListenerType = {
+  balance: "runForestFeedEventListener",
+  mirror: "runLensEventListener",
+};
 
 @Injectable()
 export class Listener {
-
   constructor(
     private web3Service: Web3Service,
     private configService: ConfigService,
-    private eventService:EventService
-    // private bugsnag: BugsnagService,
+    private eventService: EventService,
+    private bugSnag: BugsnagService
   ) {}
 
   @Command({
@@ -47,15 +49,19 @@ export class Listener {
       type: "string",
       required: false,
     })
-    type: string = "mirror",
+    type: string = "mirror"
   ) {
-    console.log("Web3 Instance Created !!",type);
+    console.log("Web3 Instance Created !!", type);
 
-    this.web3Service.createWeb3SInstance("",this[ListenerType[type]].bind(this));
+    this.web3Service.createWeb3SInstance(
+      "",
+      this[ListenerType[type]].bind(this)
+    );
   }
 
-  async runLensEventListener(web3){
+  //------------------------> lens listener
 
+  async runLensEventListener(web3) {
     const contracts = [
       {
         name: this.configService.get<string>("LENS_CONTRACT"),
@@ -65,14 +71,13 @@ export class Listener {
       },
     ];
 
-    let ethereumEvents = new EthereumEvents(web3, contracts,{
-      pollInterval:ListenerConfig.POLL_INTERVAL,
-      confirmations:ListenerConfig.CONFIRMATIONS,
-      chunkSize:ListenerConfig.CHUNK_SIZE,
-      concurrency:ListenerConfig.CONCURRENCY,
-      backoff:ListenerConfig.BACK_OFF,
+    let ethereumEvents = new EthereumEvents(web3, contracts, {
+      pollInterval: ListenerConfig.POLL_INTERVAL,
+      confirmations: ListenerConfig.CONFIRMATIONS,
+      chunkSize: ListenerConfig.CHUNK_SIZE,
+      concurrency: ListenerConfig.CONCURRENCY,
+      backoff: ListenerConfig.BACK_OFF,
     });
-    
 
     ethereumEvents.start(
       await this.eventService.loadLastState(LastStateIds.MIRROR)
@@ -80,57 +85,43 @@ export class Listener {
 
     let lastErrorTime = new Date();
 
+    ethereumEvents.on("block.confirmed", async (blockNumber, events, done) => {
+      console.log("block.confirmed", blockNumber);
 
-    ethereumEvents.on(
-      "block.confirmed",
-      async (blockNumber, events, done) => {
-        
-        console.log("block.confirmed", blockNumber, events);
+      let res = await new Promise(async (resolve, reject) => {
+        if (events.length > 0) {
+          for (let event of events) {
+            if (event.name === LensEventName.Mirror_CREATED) {
+              try {
+                ///-------------------> lens service
+              } catch (error) {
+                reject(error);
+                console.log("TREE_ASSIGNED error", error);
+              }
+            }
+          }
+        }
+        resolve("done");
+      }).catch((e) => {
+        this.bugSnag.notify(
+          "Mirror event listener (service side) error :  " + e
+        );
+        done("err");
+      });
 
-        // lastErrorTime = new Date();
+      if (res) {
+        try {
+          await this.eventService.saveLastState(
+            LastStateIds.MIRROR,
+            blockNumber
+          );
+        } catch (e) {}
 
-        // await new Promise(async (resolve, reject) => {
-        //   if (events.length > 0) {
-        //     for (let event of events) {
-        //       if (event.name === EventName.TREE_ASSIGNED) {
-        //         try {
-        //           await this.plantVerificationService.verifyAssignedTree(
-        //             Number(event.values.treeId),
-        //           );
-        //         } catch (error) {
-        //           console.log("TREE_ASSIGNED error", error);
-        //         }
-        //       } else if (event.name === EventName.TREE_PLANT) {
-        //         try {
-        //           await this.plantVerificationService.verifyPlant(
-        //             event.values.planter,
-        //             Number(event.values.nonce),
-        //           );
-        //         } catch (error) {
-        //           console.log("TREE_PLANT error", error);
-        //         }
-        //       } else if (event.name === EventName.TREE_UPDATE) {
-        //         try {
-        //           await this.plantVerificationService.verifyUpdate(
-        //             Number(event.values.treeId),
-        //           );
-        //         } catch (error) {
-        //           console.log("TREE_UPDATE error", error);
-        //         }
-        //       }
-        //     }
-        //   }
-
-        //   await this.plantVerificationService.saveLastState(blockNumber);
-
-        //   resolve("done");
-        // });
-        
         done();
-      },
-    );
+      }
+    });
 
-    ethereumEvents.on('error', err => {
+    ethereumEvents.on("error", (err) => {
       const currentTime = new Date();
       const minutesToCheck = 1;
 
@@ -138,27 +129,27 @@ export class Listener {
         (currentTime.getTime() - lastErrorTime.getTime()) / (1000 * 60)
       );
 
-      console.log("not time",diffMinutes)
-      
-      if (diffMinutes >= minutesToCheck) {
+      console.log("not time", diffMinutes);
 
+      if (diffMinutes >= minutesToCheck) {
         lastErrorTime = currentTime;
 
-        // this.bugsnag.notify("it's timeeeeeeeeeeeeeeeeee    " + err);
+        this.bugSnag.notify("Mirror event listener error : " + err);
 
-        console.log("it's timeeeeeeeeeeeeeeeeee")
-      
-        this.web3Service.createWeb3SInstance('',this.runLensEventListener.bind(this));  
+        console.log("it's timeeeeeeeeeeeeeeeeee");
+
+        this.web3Service.createWeb3SInstance(
+          "",
+          this.runLensEventListener.bind(this)
+        );
       }
-  
     });
 
     return ethereumEvents;
-      
   }
 
+  //------------------------> get balance listener
 
-  
   async runForestFeedEventListener(web3) {
     const contracts = [
       {
@@ -173,15 +164,17 @@ export class Listener {
       },
     ];
 
-    let ethereumEvents = new EthereumEvents(web3, contracts,{
-      pollInterval:ListenerConfig.POLL_INTERVAL,
-      confirmations:ListenerConfig.CONFIRMATIONS,
-      chunkSize:ListenerConfig.CHUNK_SIZE,
-      concurrency:ListenerConfig.CONCURRENCY,
-      backoff:ListenerConfig.BACK_OFF,
+    let ethereumEvents = new EthereumEvents(web3, contracts, {
+      pollInterval: ListenerConfig.POLL_INTERVAL,
+      confirmations: ListenerConfig.CONFIRMATIONS,
+      chunkSize: ListenerConfig.CHUNK_SIZE,
+      concurrency: ListenerConfig.CONCURRENCY,
+      backoff: ListenerConfig.BACK_OFF,
     });
 
-    ethereumEvents.start(await this.eventService.loadLastState(LastStateIds.BALANCE));
+    ethereumEvents.start(
+      await this.eventService.loadLastState(LastStateIds.BALANCE)
+    );
 
     let lastErrorTime = new Date();
 
@@ -190,7 +183,36 @@ export class Listener {
 
       lastErrorTime = new Date();
 
-      done();
+      let res = await new Promise(async (resolve, reject) => {
+        if (events.length > 0) {
+          for (let event of events) {
+            if (event.name === ForestFeedEventName.DEPOSITED) {
+              try {
+                //---> forest feed service
+              } catch (error) {
+                reject("error");
+                console.log("DEPOSITED error", error);
+              }
+            }
+          }
+        }
+        resolve("done");
+      }).catch((e) => {
+        this.bugSnag.notify(
+          "Balance event listener (service side) error : " + e
+        );
+        done("err");
+      });
+
+      if (res) {
+        try {
+          await this.eventService.saveLastState(
+            LastStateIds.BALANCE,
+            blockNumber
+          );
+        } catch (e) {}
+        done();
+      }
     });
 
     ethereumEvents.on("error", (err) => {
@@ -206,7 +228,7 @@ export class Listener {
       if (diffMinutes >= minutesToCheck) {
         lastErrorTime = currentTime;
 
-        // this.bugsnag.notify("it's timeeeeeeeeeeeeeeeeee    " + err);
+        this.bugSnag.notify("Balance event listener error : " + err);
 
         console.log("it's timeeeeeeeeeeeeeeeeee");
 
@@ -219,5 +241,4 @@ export class Listener {
 
     return ethereumEvents;
   }
-
 }
