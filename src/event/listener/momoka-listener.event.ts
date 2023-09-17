61e3ce21ad9b8f2f38d4de8ebd63815bef6e7f88
Mahdi Ghorbanzadeh <mahdigorbanzadeh.1378@yahoo.com>
2023-09-17 22:07:54 +0330

    add momoka listener

---
+++ src/event/listener/momoka-listener.event.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class MomokaListener {
+  constructor() {}
+
+  async runMomokaListener() {
+    //check the stack is emp
+
+    if (stack == empty) {
+      let error = 0;
+      while (true) {
+        try {
+          if (error == 20) {
+            break;
+          }
+          // request to get 50 transactions of momoka
+          let requests = [];
+
+          requests.forEach((element) => {});
+        } catch (e) {
+          error++;
+        }
+      }
+    }
+  }
+}
